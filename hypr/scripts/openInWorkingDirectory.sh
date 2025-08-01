8fdc51ebcb730a6084360dff7a4d683d07e8474c
Andrew Schlabach <andrew.schlabach@hotmail.com>
2025-08-01 13:21:54 -0400

    8-1-2025 backup

---
+++ hypr/scripts/openInWorkingDirectory.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+myterminal=kitty # it would be better if this pulled from the omarchy $terminal
+myshell=zsh
+if [ $(hyprctl activewindow | rg 'class' | awk '{print $2}') == "$myterminal" ]; then
+
+  pid=$(hyprctl activewindow | rg 'pid' | awk '{print $2}')
+  # echo $processName > ~/debugtermcommand.txt
+  # echo $pid >> ~/debugtermcommand.txt
+  # echo $(pgrep -P $pid) >> ~/debugtermcommand.txt
+  # echo $(ps $pid) >> ~/debugtermcommand.txt
+  for word in $(pgrep -P $pid); do
+    # echo "ps words are $(ps $word)" >> ~/debugtermcommand.txt
+    if [[ "$(ps $word)" == *$myshell* ]] ; then 
+      # echo "zsh pid is: $word" >> ~/debugtermcommand.txt
+      # echo "zsh wd is: $(readlink /proc/$word/cwd)" >> ~/debugtermcommand.txt
+      uwsm app -- $myterminal $(readlink /proc/$word/cwd) &
+      # echo $(readlink /proc/$word/cwd)
+      exit 0
+    fi
+  done
+fi
+
+#default case
+uwsm app -- $myterminal
+
